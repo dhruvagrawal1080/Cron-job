88f1bd0fc0d60976f710e3cfda9e6733a29441f3
Dhruv agrawal <dhruvagrawal1080@gmail.com>
2025-08-28 16:30:05 +0530

    Update cron job URL to use the production endpoint

--- apis.js
+++ apis.js
@@ -1,5 +1,5 @@
 module.exports = [
-  { name: "cron job", url: "http://localhost:3000/ping" },
+  { name: "cron job", url: "https://cron-job-yman.onrender.com/ping" },
   { name: "interview ai", url: "https://interviewai-00l5.onrender.com/ping" },
   { name: "pdf chat ai", url: "https://pdf-chat-bfs0.onrender.com/ping" },
 ];
